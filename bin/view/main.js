2646c833318b17acfcc6128bd0366deda294aca0
Kenta Kubota <zingl.0523@gmail.com>
2020-03-27 05:15:51 +0000

    キーワードリストの表示 main.js template.htmlを変更 link_keywords.cgiを追加

--- bin/view/main.js
+++ bin/view/main.js
@@ -1,8 +1,7 @@
-
-
 //ブラウザがCGIを表示したら実行
 window.onload = function () {
     lastArticles(10);
+    linkKeywords();
 }
 
 function lastArticles(num){
@@ -16,10 +15,27 @@ function lastArticles(num){
         //HTMLのlast-articlesというIDを持つ要素の中(innerHTML)にresponseTextを流し込む
         document.getElementById("last-articles").innerHTML = httpReq.responseText;
     }
+    //cgiを実行
     var url = "/last_articles.cgi?num=" + num;
     httpReq.open("GET",url,true);
     //送信する
     httpReq.send(null);
 }
 
+
+// lastArticlesほぼ同等
+function linkKeywords(){
+    var httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function(){
+        if(httpReq.readyState != 4 || httpReq.status != 200)
+            return;
+
+        document.getElementById("keywords").innerHTML = httpReq.responseText;
+    }
+    var word = document.getElementById("keywords").innerHTML;
+    //cgiを実行 日本語や記号を送信するためにencodeURIComponentを使う
+    var url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
+    httpReq.open("GET",url,true);
+    httpReq.send(null);
+}
 
